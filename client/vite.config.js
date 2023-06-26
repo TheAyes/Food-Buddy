1c27df8c17ec03ce922898b0bd3b98eb65a18bd0
Justin Schildt <Just.schildt@gmail.com>
2023-06-26 14:08:19 +0200

    Added proxy to dev server

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
 	server: {
 		// eslint-disable-next-line no-undef
 		port: process.env.CLIENT_PORT ? Number(process.env.CLIENT_PORT) : 3000,
-		host: "localhost",
 
 		proxy: {
 			"/api": {
