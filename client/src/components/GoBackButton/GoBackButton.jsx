7819e9470a3d2bb33e90aa3b99092b6109cecaa7
supermazlum <mazlum.superc@gmail.com>
2023-06-27 13:14:36 +0200

    korrektur GoBackButton und klammern weg von Data

--- client/src/components/GoBackButton/GoBackButton.jsx
+++ client/src/components/GoBackButton/GoBackButton.jsx
@@ -3,11 +3,6 @@ import { useNavigate } from 'react-router-dom';
 
 export const GoBackButton = () => {
     const navigate = useNavigate();
-    const history = useHistory();
-
-    const goBack = () => {
-        history.goBack();
-    };
 
     return (
         // in einen Pfeil nach links umändern
