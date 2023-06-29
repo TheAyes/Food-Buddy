099d8d1f6ac0dd704f37034931198075f6f9bc47
supermazlum <mazlum.superc@gmail.com>
2023-06-29 17:20:30 +0200

    updating changes

--- client/src/pages/WishList/wishlist-page.jsx
+++ client/src/pages/WishList/wishlist-page.jsx
@@ -1,14 +1,16 @@
 import styles from './wishlist-page.module.scss';
 import { GoBackButton } from "../../components/GoBackButton/GoBackButton";
+
 // import React, { useState } from 'react';
 
 export const WishlistPage = () => {
-    return
-    <section className={styles.wishListPage}>
-        <div>
-            <GoBackButton />
-            {/* Titel erstellen */}
-            {/* Favorites Button */}
-        </div>
-    </section>
+    return (
+        <section className={styles.wishlistPage}>
+            <div className={styles.headerWishlist}>
+                <GoBackButton />
+                <h4 >Wishlist</h4>
+                {/* Löschen Button */}
+            </div>
+        </section>
+    )
 };
