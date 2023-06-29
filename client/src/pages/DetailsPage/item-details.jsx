c531ed53f8d2ca027e2a360115227d0ba198b76d
supermazlum <mazlum.superc@gmail.com>
2023-06-29 09:11:43 +0200

    styling item details

--- client/src/pages/DetailsPage/item-details.jsx
+++ client/src/pages/DetailsPage/item-details.jsx
@@ -1,6 +1,11 @@
-import styles from './item-details.module.scss';
-import { GoBackButton } from "../../components/GoBackButton/GoBackButton";
 import React, { useState } from 'react';
+import { GoBackButton } from "../../components/GoBackButton/GoBackButton";
+import styles from './item-details.module.scss';
+
+// Bilder Import
+import starImage from "../../pics/star.svg";
+import cartImage from "../../pics/shopping-cart.svg";
+import heartImage from "../../pics/likeButton.svg";
 
 export const ItemDetails = (props) => {
 	const [quantity, setQuantity] = useState(0);
@@ -15,16 +20,16 @@ export const ItemDetails = (props) => {
 
 	const addToCart = () => {
 		console.log(`Menge ${quantity} zum Einkaufswagen hinzugefügt!`);
-		// Füge hier die Logik zum Hinzufügen der ausgewählten Menge in den Einkaufswagen hinzu
 	};
 
 	return (
 		<section className={styles.itemDetails}>
 			<div className={styles.upperPart}>
 				<GoBackButton />
-				<h4 className={styles.pageTitle}>testName</h4>
 				{/* Titel erstellen */}
+				<h4 className={styles.pageTitle}>testName</h4>
 				{/* Favorites Button */}
+				<img src={heartImage} alt="like Button" />
 			</div>
 			<article className={styles.imageSection}>
 				<img src="https://source.unsplash.com/random/1600x900"
@@ -35,7 +40,7 @@ export const ItemDetails = (props) => {
 				<h2 className={styles.priceIndicator}>99,99€{props.price}</h2>
 				<h3 className={styles.nameIndicator}>testProdukt{props.name}</h3>
 				<div className={styles.ratingSection}>
-					{/* Sternsymbol für Ratings */}
+					<img src={starImage} alt="rating star" />
 					<h3 className={styles.ratingIndicator}>9,9{props.rating}</h3>
 					<h3 className={styles.reviewIndicator}>(999 Reviews){props.numOfRatings}</h3>
 				</div>
@@ -48,10 +53,14 @@ export const ItemDetails = (props) => {
 					<p className={styles.numberQuantity}>{quantity}</p>
 					<button className={styles.plusQuantity} onClick={incrementQuantity}>+</button>
 				</div>
-				{/* {Einkaufswagensymbol einbauen} */}
-				<button className={styles.addCartButton} onClick={addToCart}>Add to Cart</button>
+				<article className={styles.shoppingSection}>
+					<img src={cartImage} alt="cart image" />
+					<p>99</p>
+					<button className={styles.addCartButton} onClick={addToCart}>Add to Cart</button>
+				</article>
 			</div>
 		</section>
 	);
 };
+
 
