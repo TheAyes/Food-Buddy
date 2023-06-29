469551d6be09119eb20a460f1f99cd188f430835
supermazlum <mazlum.superc@gmail.com>
2023-06-29 14:12:31 +0200

    wishlist not ready yet

--- client/src/pages/WishList/wishlist-page.jsx
+++ client/src/pages/WishList/wishlist-page.jsx
@@ -1,3 +1,14 @@
+import styles from './wishlist-page.module.scss';
+import { GoBackButton } from "../../components/GoBackButton/GoBackButton";
+// import React, { useState } from 'react';
+
 export const WishlistPage = () => {
-	return <></>;
+	return
+	<section className={styles.wishListPage}>
+		<div>
+			<GoBackButton />
+			{/* Titel erstellen */}
+			{/* Favorites Button */}
+		</div>
+	</section>
 };
