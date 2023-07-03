5fd71a071f35258cd7875ec2011476a7ff79c461
Bano <truong.supercode@gmail.com>
2023-07-03 10:39:28 +0200

    go

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -1,6 +1,39 @@
 import React, { useState } from "react";
 
 export const Register = () => {
-	return <></>;
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [email, setEmail] = useState("");
+
+	const handleRegister = () => {
+		//Hier der Code für die Registrierung
+	};
+
+	return (
+		<div>
+			<h1>Create New Account</h1>
+			<h2>Enter your details to create account</h2>
+			<form action="login" className={styles.form}>
+				<div>
+					<p>Email</p>
+					<input
+						type="text"
+						placeholder="Email adress.."
+						value={username}
+						onChange={(e) => setUsername(e.target.value)}
+					/>
+				</div>
+				<div>
+					<p>Password</p>
+					<input
+						type="password"
+						placeholder="Your Password.."
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+				</div>
+			</form>
+		</div>
+	);
 };
 
