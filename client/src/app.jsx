b6222238cec2f0bbeec9d596dd10336dbb7a718b
Justin Schildt <Just.schildt@gmail.com>
2023-07-03 15:28:57 +0200

    Upadted package lock

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -10,13 +10,7 @@ import { OrderHistoryPage } from "./pages/OrderHistory/order-history-page.jsx";
 import { ItemList } from "./pages/ItemList/item-list.jsx";
 import { Register } from "./pages/Register/Register.jsx";
 import { Login } from "./pages/Login/Login.jsx";
-import { FilterPage } from "./pages/Filter/filter-page.jsx";
-import { NavBar } from "./components/NavBar/NavBar.jsx";
-import { LikeButton } from "./components/LikeButton/LikeButton.jsx";
-import { AddToCartComponent } from "./components/AddToCartComponent/AddToCartComponent.jsx";
-import { CategoryBarImage } from "./components/CategoryBarImage/CategoryBarImage.jsx";
 import { LandPageTwo } from "./pages/LandPageTwo/land-page-two.jsx";
-import { WishItem } from "./components/WishItem/WishItem.jsx";
 
 export const App = () => {
 	return (
@@ -24,7 +18,7 @@ export const App = () => {
 			<ItemList />
 			<Routes>
 				<Route path="/" element={<LandingPage />} />
-				<Route path="/landignpagetwo" element={<LandingPageTwo />} />
+				<Route path="/landignpagetwo" element={<LandPageTwo />} />
 				<Route path="/login" element={<Login />} />
 				<Route path="/register" element={<Register />} />
 				<Route path="/home" element={<HomePage />} />
