5826443fdd3c90d714fd2c788129386bdb373629
Justin Schildt <Just.schildt@gmail.com>
2023-07-04 23:14:30 +0200

    @Vanessa - ItemList.jsx Added Category and Filter params to route

--- client/src/pages/ItemList/item-list.jsx
+++ client/src/pages/ItemList/item-list.jsx
@@ -2,22 +2,43 @@ import React, { useEffect, useState } from "react";
 import { ProductItems } from "../../components/ProductItems/ProductItems.jsx";
 import { SearchBar } from "../../components/SearchBar/SearchBar.jsx";
 import styles from "./ItemList.module.scss";
+import axios from "axios";
 
-export const ItemList = () => {
+export const ItemList = ({
+	category = "",
+	nameFilter = "",
+	minPrize = undefined,
+	maxPrize = undefined,
+	minRating = undefined,
+	maxRating = undefined,
+	minNumberOfRatings = "undefined",
+	maxNumberOfRatings = "undefined"
+}) => {
 	const [filteredData, setFilteredData] = useState([]);
 
 	useEffect(() => {
+		const params = new URLSearchParams();
+
+		// Append parameters if they exist
+		if (category) params.append("category", category);
+		if (nameFilter) params.append("nameFilter", nameFilter);
+		if (minPrize) params.append("minPrize", minPrize);
+		if (maxPrize) params.append("maxPrize", maxPrize);
+		if (minRating) params.append("minRating", minRating);
+		if (maxRating) params.append("maxRating", maxRating);
+		if (minNumberOfRatings) params.append("minNumberOfRatings", minNumberOfRatings);
+		if (maxNumberOfRatings) params.append("maxNumberOfRatings", maxNumberOfRatings);
+
 		// Selbst ausführende Funktion
 		(async () => {
 			try {
-				const response = await fetch("/api/products"); // Endpunkt anpassen
-				const data = await response.json();
+				const { data } = await axios.get(`/api/products?${params}`);
 				setFilteredData(data);
 			} catch (error) {
 				console.error("Error:", error);
 			}
 		})(); // <-- Siehe funktionsklammern
-	}, []);
+	}, [category, nameFilter, minPrize, maxPrize, minRating, maxRating, minNumberOfRatings, maxNumberOfRatings]);
 
 	const handleSelectItem = (item) => {
 		const filteredItems = filteredData.filter((dataItem) =>
@@ -25,7 +46,7 @@ export const ItemList = () => {
 		);
 		setFilteredData([...filteredItems, item]);
 	};
-	
+
 	return (
 		//hier das DIV nur als Übung für SearchBar
 		<div>
