6f7bdc545bc83c05444f35e9f979f055708fa7c4
Justin Schildt <Just.schildt@gmail.com>
2023-07-05 10:58:11 +0200

    Fixed item id not working

--- client/src/components/DealList/DealList.jsx
+++ client/src/components/DealList/DealList.jsx
@@ -1,40 +1,35 @@
-import React, { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import styles from "./DealList.module.scss";
 import { DealItems } from "../DealItems/Dealitems";
-
+import axios from "axios";
 
 export const DealList = ({ amount, offset = 0 }) => {
-    const [products, setProducts] = useState([]);
+	const [products, setProducts] = useState([]);
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+	useEffect(() => {
+		(async () => {
+			try {
+				const { data } = await axios.get("/api/products");
+				if (!data) return console.error("Error:", "Invalid Response!");
+				setProducts(data.slice(offset, offset + amount)); // Nur die ersten 6 Produkte
+			} catch (error) {
+				console.error("Error:", error);
+			}
+		})();
+	}, [amount, offset]);
 
-    const fetchData = async () => {
-        try {
-            const response = await fetch("/api/products");
-            if (response.ok) {
-                const data = await response.json();
-                setProducts(data.slice(offset, offset + amount)); // Nur die ersten 6 Produkte
-            } else {
-                throw new Error("Error: " + response.status);
-            }
-        } catch (error) {
-            console.error("Error:", error);
-        }
-    };
-
-    return (
-        <div className={styles.DealList}>
-            {products.map((item) => (
-                <DealItems
-                    key={item._id}
-                    image={item.image}
-                    name={item.name}
-                    price={item.price}
-                    rating={item.ratings}
-                />
-            ))}
-        </div>
-    );
+	return (
+		<div className={styles.DealList}>
+			{products.map((item) => (
+				<DealItems
+					key={item._id}
+					image={item.image}
+					name={item.name}
+					price={item.price}
+					rating={item.ratings}
+					id={item._id}
+				/>
+			))}
+		</div>
+	);
 };
