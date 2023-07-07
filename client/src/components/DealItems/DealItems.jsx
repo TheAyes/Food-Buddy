06c335fa2ed4c795ee871dbd1012b29c70064f22
supermazlum <mazlum.superc@gmail.com>
2023-07-07 09:53:13 +0200

    syling itemdetails, dealitem, productitem und weitere korrekturen

--- client/src/components/DealItems/DealItems.jsx
+++ client/src/components/DealItems/DealItems.jsx
@@ -9,19 +9,21 @@ export const DealItems = (props) => {
 	console.log(props);
 
 	return (
-		<Link to={`/item/${props.id}`} className={styles.DealItems}>
+		<div className={styles.DealItems}>
 			<LikeButton className={styles.likeButton} />
 			<div>
-				<img src={props.image} alt="image" className={styles.itemImages} />
-				<p className={styles.productName}>{props.name}</p>
-				<div className={styles.flex}>
-					<p>
-						{value} {unit}
-					</p>
-					<img src={star} className={styles.star} />
-					<p> {props.rating}</p>
-				</div>
+				<Link to={`/item/${props.id}`} >
+					<img src={props.image} alt="image" className={styles.itemImages} />
+					<p className={styles.productName}>{props.name}</p>
+					<div className={styles.flex}>
+						<p>
+							{value} {unit}
+						</p>
+						<img src={star} className={styles.star} />
+						<p> {props.rating}</p>
+					</div>
+				</Link>
 			</div>
-		</Link>
+		</div >
 	);
 };
