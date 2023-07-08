c461187c447b37e30da05c29e207621a83028d62
Bano <truong.supercode@gmail.com>
2023-07-08 21:58:52 +0200

    ok

--- client/src/components/AddToCartComponent/AddToCartComponent.jsx
+++ client/src/components/AddToCartComponent/AddToCartComponent.jsx
@@ -1,12 +1,39 @@
 import styles from "./AddToCartComponent.module.scss";
-import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import { UserContext } from "../../app";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
-export const AddToCartComponent = ({ quantity, addToCart }) => {
+export const AddToCartComponent = ({ initialAdd = false, id }) => {
+	const [addedToCart, setAddedToCart] = useState(inititalAdd);
+	const userState = useContext(UserContext);
+	const navigate = useNavigate();
+
+	const handleItemClick = async () => {
+		try {
+			await axios.post(
+				`/api/products/${id}/cart`,
+				{},
+				{
+					headers: {
+						Authorization: `Bearer ${userState.get.accessToken}`
+					}
+				}
+			);
+			setAddedToCart(true);
+			onAddToCart({ id, quantity });
+		} catch (error) {
+			// Fehlerbehandlung
+		}
+	};
+
 	return (
 		<>
-			<button className={styles.addCartButton} onClick={addToCart}>
-				ADD TO CART
+			<button className={styles.addCartButton} onClick={handleItemClick} disabled={addedToCart}>
+				{addedToCart ? "Added to Cart" : "Add to Cart"}
 			</button>
 		</>
 	);
 };
+
