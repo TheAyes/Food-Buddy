cc9f7a004e57169a0d0e949914c5195fd5ec38b5
supermazlum <mazlum.superc@gmail.com>
2023-07-08 16:24:39 +0200

    forgotten variable in wishlistpage

--- client/src/pages/WishList/wishlist-page.jsx
+++ client/src/pages/WishList/wishlist-page.jsx
@@ -1,8 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import styles from "./wishlist-page.module.scss";
 
-import { CartItem } from "../../components/CartItem/CartItem.jsx"
-
 //Import Components
 import { GoBackButton } from "../../components/GoBackButton/GoBackButton.jsx";
 import { WishItem } from "../../components/WishItem/WishItem.jsx";
@@ -70,11 +68,10 @@ export const WishlistPage = () => {
 						numOfRatings={item.ratings.length}
 						price={item.price.value}
 						onLikeButtonClick={handleLikeButtonClick}
-						isLiked={true} // Setze isLiked auf true, da sich der Artikel in der Wishlist befindet
+						isLiked={true}
 					/>
 				))}
 			</div>
-			<CartItem />
 			<NavBar />
 		</section>
 	);
